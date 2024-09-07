800dcb55f082243934c49db295a8c29e80e3357d
Xiangyi Meng <xymeng16@gmail.com>
2024-09-08 02:26:31 +0800

    win build script

--- build.ps1
+++ build.ps1
@@ -8,16 +8,21 @@ $env:GOASM = "$TOOLCHAIN/bin/llvm-as"
 $env:GOOS = "android"
 $env:GOARCH = "arm64"
 $env:GOARM = ""
-$env:CGO_ENABLED = "1"
-$env:CGO_CXXFLAGS=""
+$env:CGO_ENABLED = "0"
 
-$env:CGO_CFLAGS = "-Wno-error --target=aarch64-linux-ohos $BASE_FLAGS"
-$env:CGO_LDFLAGS = "-extld=$env:LD --sysroot=C:/ohos_sdk/native/sysroot --target=aarch64-linux-ohos"
+$env:BASE_FLAGS = "--sysroot=C:/ohos_sdk/native/sysroot -fdata-sections -ffunction-sections -funwind-tables
+-fstack-protector-strong -no-canonical-prefixes -fno-addrsig -Wa,--noexecstack -fPIC"
+
+$env:CGO_CFLAGS = "-Wno-error --target=aarch64-linux-ohos $env:BASE_FLAGS"
+$env:CGO_CXXFLAGS=$env:CGO_CFLAGS
+$env:CGO_LDFLAGS = "-extar=$env:AR -extld=$env:LD --sysroot=C:/ohos_sdk/native/sysroot --target=aarch64-linux-ohos"
 
 $TARGET_DIR = "aarch64-linux-ohos"
-$outputFile = "build/$TARGET_DIR/libsb.so"
+$OUTPUT = "build/$TARGET_DIR/libsb.so"
+$OUTPUT_EXEC = "build/$TARGET_DIR/sb"
 # compress so
 # -trimpath -ldflags="-s -w"
 go env
 Pause
-go build -buildmode c-shared -v -x -o outputFile ./cmd/sing-box
+#go build -buildmode c-shared -v -x -o $OUTPUT ./cmd/sing-box
+go build -v -x -o $OUTPUT_EXEC ./cmd/sing-box
